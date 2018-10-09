73710c93bbd77dd8dd76b5bf7089dead272fc6ed
A L Manning <asher@zenprotocol.com>
2018-10-09 15:20:29 +0300

    Improve tests, update README

---
+++ Bet/README.md
@@ -0,0 +1,3 @@
+
+To build, run `zebra -c Tests/Buy.fsx`
+To run tests, run `zebra -r Tests/Buy.fsx`
--- Bet/Tests/Buy.fsx
+++ Bet/Tests/Buy.fsx
@@ -51,12 +51,97 @@ let contractFn (txSkeleton: TxSkeleton.T) messageBody =
 // Empty transaction
 let emptyTxSkeleton : TxSkeleton.T = TxSkeleton.empty
 
+// all spends in the input
+let rec inputSpends = function
+    | input::inputs ->
+        match input with
+        | TxSkeleton.PointedOutput (_, output) ->
+            output.spend :: inputSpends inputs
+        | _ ->
+            inputSpends inputs
+    | [] -> []
+
+// all spends in the output
+let outputSpends = List.map (fun output -> output.spend)
+
+// spends in the input locked to contract
+let rec inputSpendsLockedToContract = function
+    | input::inputs ->
+        match input with
+        | TxSkeleton.PointedOutput (_, output)
+          when output.lock = Contract contractID ->
+            output.spend :: inputSpendsLockedToContract inputs
+        | _ ->
+            inputSpendsLockedToContract inputs
+    | [] -> []
+
+// spends in the output locked to contract
+let outputSpendsLockedToContract =
+    List.filter (fun output -> output.lock = Contract contractID)
+    >> List.map (fun output -> output.spend)
+
+// spends in the input locked to returnAddressPK
+let rec inputSpendsLockedToReturnAddressPK returnAddressPK = function
+    | input::inputs ->
+        match input with
+        | TxSkeleton.PointedOutput (_, output)
+          when output.lock = PK returnAddressPK ->
+            output.spend :: inputSpendsLockedToReturnAddressPK returnAddressPK inputs
+        | _ ->
+            inputSpendsLockedToReturnAddressPK returnAddressPK inputs
+    | [] -> []
+
+// spends in the output locked to returnAddressPK
+let outputSpendsLockedToReturnAddressPK returnAddressPK =
+    List.filter (fun output -> output.lock = PK returnAddressPK)
+    >> List.map (fun output -> output.spend)
+
+// gets a list of minting inputs
+let rec getMints = function
+    | input::inputs ->
+        match input with
+        | TxSkeleton.Mint spend ->
+            spend::getMints inputs
+        | _ ->
+            getMints inputs
+    | [] -> []
+
+// Total amount of ZP in a list of spends
+let totalZP spends =
+    spends |> List.filter (fun spend -> spend.asset = Asset.Zen)
+           |> List.sumBy (fun spend -> spend.amount)
+
+// Total amount of bull token in a list of spends
+let totalBullToken spends =
+    spends |> List.filter (fun spend -> spend.asset = bullToken)
+           |> List.sumBy (fun spend -> spend.amount)
+
+// Total amount of bear token in a list of spends
+let totalBearToken spends =
+    spends |> List.filter (fun spend -> spend.asset = bearToken)
+           |> List.sumBy (fun spend -> spend.amount)
+
+// a messageBody with no return address
+let noReturnAddress: data option = None
+
+let returnAddressPK = Hash.zero
+
+let returnAddress = PK returnAddressPK
+                    |> ZFStar.fsToFstLock
+                    |> Lock
+
+// a messageBody consisting of only a return address
+let onlyReturnAddress =
+    Zen.Dictionary.add "returnAddress"B returnAddress Zen.Dictionary.empty
+    |> Cost.__force
+    |> Dict
+    |> Collection
+    |> Some
+
 
 //////////////////////////////////////////////////////////////////////////
 // Buy without returnAddress fails
 //////////////////////////////////////////////////////////////////////////
-
-let noReturnAddress: data option = None
 
 match contractFn emptyTxSkeleton noReturnAddress with
 | Error "Could not parse returnAddress from messageBody" ->
@@ -65,19 +150,9 @@ match contractFn emptyTxSkeleton noReturnAddress with
 | Error e -> failwithf "Failed with unexpected error: `%A`" e
 
 //////////////////////////////////////////////////////////////////////////
-// Buy with returnAddress only
+// Buy with returnAddress and no inputs
 // Should mint 0 bull & bear tokens and spends them to returnAddress
 //////////////////////////////////////////////////////////////////////////
-
-let onlyReturnAddress =
-    let returnAddress = PK Hash.zero
-                        |> ZFStar.fsToFstLock
-                        |> Lock
-    Zen.Dictionary.add "returnAddress"B returnAddress Zen.Dictionary.empty
-    |> Cost.__force
-    |> Dict
-    |> Collection
-    |> Some
 
 match contractFn emptyTxSkeleton onlyReturnAddress with
 | Ok ({pInputs=pInputs; outputs=outputs}, None, Main.NoChange) -> // expect no message or state update
@@ -119,7 +194,186 @@ match contractFn emptyTxSkeleton onlyReturnAddress with
         failwithf "Expected 3 outputs but got: `%A`" pInputs
 
     // If you reach here, all is ok!
-    printfn "OK: Buy with returnAddress only"
-    
+    printfn "OK: Buy with returnAddress and no inputs"
+
+| Ok (_, msg, Main.NoChange) ->
+    failwithf "Expected no return message, but got: `%A`" msg
+| Ok (_, _, stateUpdate) ->
+    failwithf "Expected no state change, but got: `%A`" stateUpdate
+| Error e ->
+    failwithf "Failed with unexpected error: `%A`" e
+
+//////////////////////////////////////////////////////////////////////////
+// Buy with returnAddress and single 50ZP input
+// Should lock 50ZP to contract
+// Should mint 50 bull & bear tokens and spend them to returnAddress
+//////////////////////////////////////////////////////////////////////////
+
+let singleInputTxSkeleton =
+    let outpoint = {txHash=Hash.zero; index=0u}
+    let spend = {asset=Asset.Zen; amount=50UL}
+    let output = {lock=Contract contractID; spend=spend}
+    { TxSkeleton.pInputs=[TxSkeleton.PointedOutput (outpoint, output)];
+      TxSkeleton.outputs=[] }
+
+match contractFn singleInputTxSkeleton onlyReturnAddress with
+| Ok ({pInputs=pInputs; outputs=outputs}, None, Main.NoChange) -> // expect no message or state update
+    // inputs
+    let inputMints = getMints pInputs
+    let inputSpends = inputSpends pInputs
+    let inputZP = totalZP inputSpends
+    let inputSpendsLockedToContract = inputSpendsLockedToContract pInputs
+    let inputSpendsLockedToReturnAddressPK =
+        inputSpendsLockedToReturnAddressPK returnAddressPK pInputs
+    let inputZPLockedToContract = totalZP inputSpendsLockedToContract
+    let inputZPLockedToReturnAddressPK = totalZP inputSpendsLockedToReturnAddressPK
+    let inputBullTokensLockedToContract = totalBullToken inputSpendsLockedToContract
+    let inputBearTokensLockedToContract = totalBearToken inputSpendsLockedToContract
+    let inputBullTokensLockedToReturnAddressPK = totalBullToken inputSpendsLockedToReturnAddressPK
+    let inputBearTokensLockedToReturnAddressPK = totalBearToken inputSpendsLockedToReturnAddressPK
+    let minted = getMints pInputs
+    let bullTokensMinted = totalBullToken minted
+    let bearTokensMinted = totalBearToken minted
+    // Should be total of 50ZP in inputs
+    if inputZP <> 50UL
+        then failwithf "Expected 50ZP locked in input spends, but got: `%A`" pInputs
+    // Should be total of 50ZP locked to contract in inputs
+    if inputZPLockedToContract <> 50UL
+        then failwithf "Expected 50ZP locked to contract in inputs, but got: `%A`" pInputs
+    // Should be total of 50 Bull Tokens in input mints
+    if bullTokensMinted <> 50UL
+        then failwithf "Expected 50 Bull Tokens in input mints, but got: `%A`" pInputs
+    // Should be total of 50 Bear Tokens in input mints
+    if bearTokensMinted <> 50UL
+        then failwithf "Expected 50 Bear Tokens in input mints, but got: `%A`" pInputs
+
+    // outputs
+    let outputSpends = outputSpends outputs
+    let outputZP = totalZP outputSpends
+    let outputBullTokens = totalBullToken outputSpends
+    let outputBearTokens = totalBearToken outputSpends
+    let outputSpendsLockedToContract = outputSpendsLockedToContract outputs
+    let outputSpendsLockedToReturnAddressPK =
+        outputSpendsLockedToReturnAddressPK returnAddressPK outputs
+    let outputZPLockedToContract = totalZP outputSpendsLockedToContract
+    let outputZPLockedToReturnAddressPK = totalZP outputSpendsLockedToReturnAddressPK
+    let outputBullTokensLockedToContract = totalBullToken outputSpendsLockedToContract
+    let outputBearTokensLockedToContract = totalBearToken outputSpendsLockedToContract
+    let outputBullTokensLockedToReturnAddressPK = totalBullToken outputSpendsLockedToReturnAddressPK
+    let outputBearTokensLockedToReturnAddressPK = totalBearToken outputSpendsLockedToReturnAddressPK
+    // Should be total of 50ZP in outputs
+    if outputZP <> 50UL
+        then failwithf "Expected 50ZP locked in outputs, but got: `%A`" outputs
+    // Should be total of 50ZP locked to contract in outputs
+    if outputZPLockedToContract <> 50UL
+        then failwithf "Expected 50ZP locked to contract in outputs, but got: `%A`" outputs
+    // Should be total of 50 Bull Tokens in outputs
+    if outputBullTokens <> 50UL
+        then failwithf "Expected 50 Bull Tokens in outputs, but got: `%A`" outputs
+    // Should be total of 50 Bull Tokens locked to returnAddress in outputs
+    if outputBullTokensLockedToReturnAddressPK <> 50UL
+        then failwithf "Expected 50 Bull Tokens locked to returnAddress in outputs, but got: `%A`" outputs
+    // Should be total of 50 Bear Tokens in outputs
+    if outputBearTokens <> 50UL
+        then failwithf "Expected 50 Bear Tokens in outputs, but got: `%A`" outputs
+    // Should be total of 50 Bear Tokens locked to returnAddress in outputs
+    if outputBearTokensLockedToReturnAddressPK <> 50UL
+        then failwithf "Expected 50 Bear Tokens locked to returnAddress in outputs, but got: `%A`" outputs
+
+    // If you reach here, all is ok!
+    printfn "OK: Buy with returnAddress and single 50ZP input"
+
+| Ok (_, msg, Main.NoChange) ->
+    failwithf "Expected no return message, but got: `%A`" msg
+| Ok (_, _, stateUpdate) ->
+    failwithf "Expected no state change, but got: `%A`" stateUpdate
+| Error e ->
+    failwithf "Failed with unexpected error: `%A`" e
+
+//////////////////////////////////////////////////////////////////////////
+// Buy with returnAddress and multiple ZP inputs totalling 150ZP
+// Should lock 150ZP to contract
+// Should mint 150 bull & bear tokens and spend them to returnAddress
+//////////////////////////////////////////////////////////////////////////
+
+let multiInputTxSkeleton =
+    let outpoint = {txHash=Hash.zero; index=0u}
+    let spend x = {asset=Asset.Zen; amount=x}
+    let output x = {lock=Contract contractID; spend=spend x}
+    let pInput x = TxSkeleton.PointedOutput (outpoint, output x)
+    let pInputs = [pInput 40UL; pInput 20UL; pInput 50UL; pInput 30UL; pInput 10UL]
+    { TxSkeleton.pInputs=pInputs; TxSkeleton.outputs=[] }
+
+match contractFn multiInputTxSkeleton onlyReturnAddress with
+| Ok ({pInputs=pInputs; outputs=outputs}, None, Main.NoChange) -> // expect no message or state update
+    // inputs
+    let inputMints = getMints pInputs
+    let inputSpends = inputSpends pInputs
+    let inputZP = totalZP inputSpends
+    let inputSpendsLockedToContract = inputSpendsLockedToContract pInputs
+    let inputSpendsLockedToReturnAddressPK =
+        inputSpendsLockedToReturnAddressPK returnAddressPK pInputs
+    let inputZPLockedToContract = totalZP inputSpendsLockedToContract
+    let inputZPLockedToReturnAddressPK = totalZP inputSpendsLockedToReturnAddressPK
+    let inputBullTokensLockedToContract = totalBullToken inputSpendsLockedToContract
+    let inputBearTokensLockedToContract = totalBearToken inputSpendsLockedToContract
+    let inputBullTokensLockedToReturnAddressPK = totalBullToken inputSpendsLockedToReturnAddressPK
+    let inputBearTokensLockedToReturnAddressPK = totalBearToken inputSpendsLockedToReturnAddressPK
+    let minted = getMints pInputs
+    let bullTokensMinted = totalBullToken minted
+    let bearTokensMinted = totalBearToken minted
+    // Should be total of 150ZP in inputs
+    if inputZP <> 150UL
+        then failwithf "Expected 150ZP locked in input spends, but got: `%A`" pInputs
+    // Should be total of 150ZP locked to contract in inputs
+    if inputZPLockedToContract <> 150UL
+        then failwithf "Expected 150ZP locked to contract in inputs, but got: `%A`" pInputs
+    // Should be total of 150 Bull Tokens in input mints
+    if bullTokensMinted <> 150UL
+        then failwithf "Expected 150 Bull Tokens in input mints, but got: `%A`" pInputs
+    // Should be total of 150 Bear Tokens in input mints
+    if bearTokensMinted <> 150UL
+        then failwithf "Expected 150 Bear Tokens in input mints, but got: `%A`" pInputs
+
+    // outputs
+    let outputSpends = outputSpends outputs
+    let outputZP = totalZP outputSpends
+    let outputBullTokens = totalBullToken outputSpends
+    let outputBearTokens = totalBearToken outputSpends
+    let outputSpendsLockedToContract = outputSpendsLockedToContract outputs
+    let outputSpendsLockedToReturnAddressPK =
+        outputSpendsLockedToReturnAddressPK returnAddressPK outputs
+    let outputZPLockedToContract = totalZP outputSpendsLockedToContract
+    let outputZPLockedToReturnAddressPK = totalZP outputSpendsLockedToReturnAddressPK
+    let outputBullTokensLockedToContract = totalBullToken outputSpendsLockedToContract
+    let outputBearTokensLockedToContract = totalBearToken outputSpendsLockedToContract
+    let outputBullTokensLockedToReturnAddressPK = totalBullToken outputSpendsLockedToReturnAddressPK
+    let outputBearTokensLockedToReturnAddressPK = totalBearToken outputSpendsLockedToReturnAddressPK
+    // Should be total of 150ZP in outputs
+    if outputZP <> 150UL
+        then failwithf "Expected 150ZP locked in outputs, but got: `%A`" outputs
+    // Should be total of 150ZP locked to contract in outputs
+    if outputZPLockedToContract <> 150UL
+        then failwithf "Expected 150ZP locked to contract in outputs, but got: `%A`" outputs
+    // Should be total of 150 Bull Tokens in outputs
+    if outputBullTokens <> 150UL
+        then failwithf "Expected 150 Bull Tokens in outputs, but got: `%A`" outputs
+    // Should be total of 150 Bull Tokens locked to returnAddress in outputs
+    if outputBullTokensLockedToReturnAddressPK <> 150UL
+        then failwithf "Expected 150 Bull Tokens locked to returnAddress in outputs, but got: `%A`" outputs
+    // Should be total of 150 Bear Tokens in outputs
+    if outputBearTokens <> 150UL
+        then failwithf "Expected 150 Bear Tokens in outputs, but got: `%A`" outputs
+    // Should be total of 150 Bear Tokens locked to returnAddress in outputs
+    if outputBearTokensLockedToReturnAddressPK <> 150UL
+        then failwithf "Expected 150 Bear Tokens locked to returnAddress in outputs, but got: `%A`" outputs
+
+    // If you reach here, all is ok!
+    printfn "OK: Buy with returnAddress and multiple ZP inputs totalling 150ZP"
+
+| Ok (_, msg, Main.NoChange) ->
+    failwithf "Expected no return message, but got: `%A`" msg
+| Ok (_, _, stateUpdate) ->
+    failwithf "Expected no state change, but got: `%A`" stateUpdate
 | Error e ->
     failwithf "Failed with unexpected error: `%A`" e
